25707f2d3437dbccd70f89585f2e6073ac9c349c
Sergey Gromyshev 031 2004 <114712725+SergeyGromyshev2004@users.noreply.github.com>
2023-01-13 14:11:09 +0700

    Create решение.py

---
+++ 2 полугодие/решение.py
@@ -0,0 +1,12 @@
+#include <stdio.h>
+print('|N| |Статус|')
+('| ----- | |-----|')
+for i in range(1,28):
+print(f'|{i}| | |')
+i=10 
+b='10'+ b + '1'
+b=bin(i)[2:]
+b=f'{N:b}'
+print(b)
+c=int(b,2)
+print(c)
