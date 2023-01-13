9b432423526b73b5500ad58b30dae1d67e55939f
Sergey Gromyshev 031 2004 <114712725+SergeyGromyshev2004@users.noreply.github.com>
2023-01-13 14:17:28 +0700

    Update решение.py

--- 2 полугодие/решение.py
+++ 2 полугодие/решение.py
@@ -6,7 +6,15 @@ print(f'|{i}| | |')
 i=10 
 b='10'+ b + '1'
 b=bin(i)[2:]
+for N in range(516):
 b=f'{N:b}'
+if N%2 == 0:
+b+=10
+else:
+  b= '1' + b + '01'
 print(b)
 c=int(b,2)
 print(c)
+if c > 516:
+  print(N)
+  break
